e4967838b851bd0da6425b1e9ed01c3acf5f195b
Imtiaz Rahi <imtiaz.rahi@gmail.com>
2020-05-21 20:14:28 +0600

    Added custom save in KirrURL model

--- src/shortener/models.py
+++ src/shortener/models.py
@@ -1,3 +1,29 @@
+import random
+import string
 from django.db import models
 
+
+def code_generator(size=8, chars=string.ascii_letters + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
 # Create your models here.
+# noinspection SpellCheckingInspection
+class KirrURL(models.Model):
+    """Model definition for KirrURL."""
+
+    url = models.CharField(max_length=220)
+    shortcode = models.CharField(max_length=15, unique=True)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        self.shortcode = code_generator()
+        super(KirrURL, self).save(*args, **kwargs)
+
+    class Meta:
+        verbose_name = 'KirrURL'
+        verbose_name_plural = 'KirrURL'
+
+    def __str__(self):
+        """Unicode representation of KirrURL."""
+        return str(self.url)
